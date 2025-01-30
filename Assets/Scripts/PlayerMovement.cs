fe6b3bbc4bf0b4f37755791f685b1df9a4c233b3
Kelvinxu28 <Kelvinxu28@gmail.com>
2025-01-29 22:55:00 -0800

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,98 @@
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+private bool doublejump;
+    Rigidbody2D rb;
+    
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpHeight =  10f ;
+
+    float direction = 0;
+    bool isGrounded = false;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move(direction);
+    }
+
+    void OnMove(InputValue value)
+    {
+        float v = value.Get<float>();
+        Debug.Log(v);
+        direction = v;
+    }
+
+    void Move(float dir)
+    {
+        rb.linearVelocity = new Vector2(dir * speed, rb.linearVelocity.y);
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            rb.linearVelocity = new Vector2(dir * (3*speed), rb.linearVelocityY);
+            Debug.Log("sprinting");
+        }
+    }
+
+
+    void OnJump()
+    {
+        if(isGrounded && !Input.GetKey(KeyCode.Space))
+        {
+            doublejump = false;
+        }
+        if(isGrounded || doublejump)
+        {
+            Debug.Log("jump active");
+            Jump();
+            doublejump = !doublejump;
+        }
+    
+    }
+
+    void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpHeight);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        isGrounded = false;
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            if (Vector2.Angle(collision.GetContact(i).normal, Vector2.up) < 45f)
+            {
+                isGrounded = true;
+            }
+        }
+
+    }
+
+    void OnCollisionExit2D(Collision2D collision) 
+    {
+        isGrounded = false;
+    }
+
+
+
+
+
+
+    
+    
+}
